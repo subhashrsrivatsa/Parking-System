4e2a64c924a3c10cd78ee70c6135506c78782a1f
Subhash Ravindra SRIVATSA <subhashrsrivatsa.paris@gmail.com>
2020-04-20 20:13:44 +0530

    Initial Commit with Spring boot Project setup

---
+++ Parking-System-API/src/main/java/com/subhash/parkingsystem/ParkingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.subhash.parkingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSystemApiApplication.class, args);
+	}
+
+}
---
+++ Parking-System-API/src/test/java/com/subhash/parkingsystem/ParkingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhash.parkingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
